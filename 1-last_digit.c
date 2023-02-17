03599b2b6df2a25cd75092a7fee50eff3305ae42
natalienyabanhi <nyabanhi@gmail.com>
2023-02-17 08:08:00 -0800

    1-last_digit.c

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -8,14 +8,16 @@
  *
  * Return: Always 0.
  */
+
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if ((n % 10) > 5)
+	/* your code goes there */
+if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n",
 			n, n % 10);
@@ -31,6 +33,6 @@ int main(void)
 			n, n % 10);
 	}
 
+	
 	return (0);
-
 }
