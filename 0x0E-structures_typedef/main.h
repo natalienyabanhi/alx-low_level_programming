deb17aae740d4aafa34059d81d4ce30c16bb593e
natalienyabanhi <nyabanhi@gmail.com>
2023-03-20 08:53:46 -0700

    structures

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "dog.h"
+#include "1-init_dog.c"
+#include "2-print_dog.c"
+#include "4-new_dog.c"
+#include "5-free_dog.c"
+
+
+#endif
